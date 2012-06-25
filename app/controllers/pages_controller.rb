da2b20e216cea879a9dc7231691cd1e2e1df8726
Josh Smith <joshsmith9255@gmail.com>
2012-06-25 01:37:31 -0400

    Initial commit

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,13 @@
+class PagesController < ApplicationController
+  def index
+  end
+
+  def contact
+  end
+
+  def terms
+  end
+
+  def privacy
+  end
+end
---
+++ test/functional/pages_controller_test.rb
@@ -0,0 +1,24 @@
+require 'test_helper'
+
+class PagesControllerTest < ActionController::TestCase
+  test "should get index" do
+    get :index
+    assert_response :success
+  end
+
+  test "should get contact" do
+    get :contact
+    assert_response :success
+  end
+
+  test "should get terms" do
+    get :terms
+    assert_response :success
+  end
+
+  test "should get privacy" do
+    get :privacy
+    assert_response :success
+  end
+
+end
